2350328356a66c973e14eab8d0c862a0cf449608
Darren Soulier <56926037+Kichuwa@users.noreply.github.com>
2022-01-18 23:45:58 -0600

    Day 1 entry. Majority installs was the goal.

---
+++ Day1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Day 1: learning through the week!
+//Basics in C# are relative to Java. This should speed up the process.
+namespace Day1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello all! \n" +
+                "This is my first use of C#.");
+            Console.Read();
+        }
+    }
+}
